d74996953c25a9b00b2ac5869c3e46efb5374677
Víctor Morales <vic.mor.593@gmail.com>
2018-06-04 22:05:58 -0600

    cambios

--- quiniela/src/pages/tabla/tabla.ts
+++ quiniela/src/pages/tabla/tabla.ts
@@ -9,15 +9,57 @@ import { TablaProvider } from '../../providers/tabla/tabla';
 })
 export class TablaPage {
 
-  tablas: any;
+  tablasA: any;
+  tablasB: any;
+  tablasC: any;
+  tablasD: any;
+  tablasE: any;
+  tablasF: any;
+  tablasG: any;
+  tablasH: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public tablaService: TablaProvider) {
   }
 
   ionViewDidLoad() {
-    this.tablaService.getTabla().then((data) => {
+    this.tablaService.getTablaA().then((data) => {
       console.log(data);
-      this.tablas = data;
+      this.tablasA = data;
+    });
+
+    this.tablaService.getTablaB().then((data) => {
+      console.log(data);
+      this.tablasB = data;
+    });
+
+    this.tablaService.getTablaC().then((data) => {
+      console.log(data);
+      this.tablasC = data;
+    });
+
+    this.tablaService.getTablaD().then((data) => {
+      console.log(data);
+      this.tablasD = data;
+    });
+
+    this.tablaService.getTablaE().then((data) => {
+      console.log(data);
+      this.tablasE = data;
+    });
+
+    this.tablaService.getTablaF().then((data) => {
+      console.log(data);
+      this.tablasF = data;
+    });
+
+    this.tablaService.getTablaG().then((data) => {
+      console.log(data);
+      this.tablasG = data;
+    });
+
+    this.tablaService.getTablaH().then((data) => {
+      console.log(data);
+      this.tablasH = data;
     });
   }
 
--- quiniela/src/providers/tabla/tabla.ts
+++ quiniela/src/providers/tabla/tabla.ts
@@ -11,7 +11,7 @@ export class TablaProvider {
     this.data = null;
   }
 
-  getTabla(){
+  getTablaA(){
     if (this.data) {
       return Promise.resolve(this.data);
     }
@@ -22,6 +22,118 @@ export class TablaProvider {
         .map(res => res.json())
         .subscribe(data => {
           this.data = data.standings.A;
+          resolve(this.data);
+        });
+    });
+  }
+
+  getTablaB(){
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get('http://localhost:8100/tabla')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.standings.B;
+          resolve(this.data);
+        });
+    });
+  }
+
+  getTablaC(){
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get('http://localhost:8100/tabla')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.standings.C;
+          resolve(this.data);
+        });
+    });
+  }
+
+  getTablaD(){
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get('http://localhost:8100/tabla')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.standings.D;
+          resolve(this.data);
+        });
+    });
+  }
+
+  getTablaE(){
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get('http://localhost:8100/tabla')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.standings.E;
+          resolve(this.data);
+        });
+    });
+  }
+
+  getTablaF(){
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get('http://localhost:8100/tabla')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.standings.F;
+          resolve(this.data);
+        });
+    });
+  }
+
+  getTablaG(){
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get('http://localhost:8100/tabla')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.standings.G;
+          resolve(this.data);
+        });
+    });
+  }
+
+  getTablaH(){
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get('http://localhost:8100/tabla')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.standings.H;
           resolve(this.data);
         });
     });
